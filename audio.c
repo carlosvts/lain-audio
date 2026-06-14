1ed14a8c36d4b81d35ddb341842ec7070e8c475c
carlosvts <carlosvtsdev@gmail.com>
2026-06-14 20:17:24 -0300

    feat: raylib implementation for visualization

--- audio.c
+++ audio.c
@@ -1,7 +1,35 @@
 #include "include/lain_audio.h"
 #include "include/lain_fs.h"
 #include "include/lain_io.h"
+
+#include <raylib.h>
 #include <stdio.h>
+#include <stdlib.h>
+
+#define SCREEN_WIDTH 1000
+#define SCREEN_HEIGHT 1000
+
+#define BAR_COUNT 250
+#define BAR_GAP 1.0f
+#define VISUALIZER_MAX_HEIGHT ((f32)SCREEN_HEIGHT * 0.4f)
+
+static bool read_full(i32 fd, void *buffer, usize size)
+{
+    u8 *cursor = (u8 *)buffer;
+    usize total_read = 0;
+
+    while (total_read < size)
+    {
+        isize bytes_read = lain_read(fd, cursor + total_read, size - total_read);
+
+        if (bytes_read <= 0)
+            return false;
+
+        total_read += (usize)bytes_read;
+    }
+
+    return true;
+}
 
 int main(int argc, char *argv[])
 {
@@ -10,27 +38,108 @@ int main(int argc, char *argv[])
         printf("use: ./audioinfo <path-to-wav>\n");
         return -1;
     }
+
     WavHeader wavheader;
-    WavData wavdata;
-    
+    Audio audio;
+
     char *path = argv[1];
     i32 fd = lain_open(lain_string(path));
 
-    // read wavheader
-    lain_read(fd, &wavheader, sizeof(wavheader));
-    // read wavdata
-    lain_read(fd, &wavdata, sizeof(wavdata));
+    if (fd == -1)
+        return -1;
+
+    if (!read_full(fd, &wavheader, sizeof(wavheader)))
+    {
+        lain_perror(lain_string("ERROR: unable to read wav header"));
+        lain_close(fd);
+        return -1;
+    }
 
     bool isvalid = is_wav_valid(&wavheader);
     if (!isvalid)
     {
         lain_perror(lain_string("Incorret .wav datatype, unable to parse\n"));
+        lain_close(fd);
         return -1;
     }
+
+    if (!audio_from_wav(&wavheader, &audio))
+    {
+        lain_close(fd);
+        return -1;
+    }
+
+    audio.rawdata.data = malloc(audio.rawdata.data_size);
+    if (!audio.rawdata.data)
+    {
+        lain_perror(lain_string("ERROR: unable to allocate wav data"));
+        lain_close(fd);
+        return -1;
+    }
+
+    if (!read_full(fd, audio.rawdata.data, audio.rawdata.data_size))
+    {
+        lain_perror(lain_string("ERROR: unable to read wav data"));
+        free(audio.rawdata.data);
+        lain_close(fd);
+        return -1;
+    }
+
+    lain_close(fd);
 
     printf("Num channels = %i\n", wavheader.num_channels);
     printf("Audio format = %i\n", wavheader.audio_format);
     printf("Format       = %.4s\n", wavheader.format);
+
+    InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "lain audio visualizer");
+    SetTargetFPS(60);
+
+    const f32 center_y = (f32)SCREEN_HEIGHT / 2.0f;
+    const f32 bar_width = ((f32)SCREEN_WIDTH / (f32)BAR_COUNT) - BAR_GAP;
+    const i64 frames_per_bar = audio.sample_count / BAR_COUNT;
+
+    while (!WindowShouldClose())
+    {
+        BeginDrawing();
+        ClearBackground((Color){18, 18, 22, 255});
+
+        DrawLine(0, (i32)center_y, SCREEN_WIDTH, (i32)center_y, (Color){64, 64, 72, 255});
+
+        for (i32 i = 0; i < BAR_COUNT; i++)
+        {
+            i64 start_frame = (audio.sample_count * i) / BAR_COUNT;
+            i64 end_frame = (audio.sample_count * (i + 1)) / BAR_COUNT;
+            i64 frame_count = end_frame - start_frame;
+
+            if (frame_count <= 0)
+                frame_count = frames_per_bar > 0 ? frames_per_bar : 1;
+
+            f32 peak = 0.0f;
+            for (i32 channel = 0; channel < audio.channels; channel++)
+            {
+                f32 channel_peak = audio_peak(&audio, start_frame, frame_count, channel);
+
+                if (channel_peak > peak)
+                    peak = channel_peak;
+            }
+
+            if (peak > 1.0f)
+                peak = 1.0f;
+
+            f32 height = peak * VISUALIZER_MAX_HEIGHT;
+            f32 x = (f32)i * ((f32)SCREEN_WIDTH / (f32)BAR_COUNT);
+            f32 y = center_y - height;
+
+            DrawRectangleRec(
+                (Rectangle){x, y, bar_width, height},
+                (Color){91, 214, 167, 255});
+        }
+
+        EndDrawing();
+    }
+
+    CloseWindow();
+    free(audio.rawdata.data);
 
     return 0;
 }
