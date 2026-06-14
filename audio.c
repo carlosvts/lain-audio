ba194eb6f2ba1fca3eacacda3dc7a6c8352da4ff
carlosvts <carlosvtsdev@gmail.com>
2026-06-14 20:22:31 -0300

    trying to make rendering music dynamic

--- audio.c
+++ audio.c
@@ -7,11 +7,12 @@
 #include <stdlib.h>
 
 #define SCREEN_WIDTH 1000
-#define SCREEN_HEIGHT 1000
+#define SCREEN_HEIGHT 500 
 
 #define BAR_COUNT 250
 #define BAR_GAP 1.0f
 #define VISUALIZER_MAX_HEIGHT ((f32)SCREEN_HEIGHT * 0.4f)
+#define VISUALIZER_SECONDS 3.0f
 
 static bool read_full(i32 fd, void *buffer, usize size)
 {
@@ -29,6 +30,49 @@ static bool read_full(i32 fd, void *buffer, usize size)
     }
 
     return true;
+}
+
+static void draw_visualizer(const Audio *audio, f32 played_seconds)
+{
+    const f32 center_y = (f32)SCREEN_HEIGHT / 2.0f;
+    const f32 bar_width = ((f32)SCREEN_WIDTH / (f32)BAR_COUNT) - BAR_GAP;
+    const i64 visible_frames = (i64)((f32)audio->sample_rate * VISUALIZER_SECONDS);
+    const i64 playhead_frame = (i64)(played_seconds * (f32)audio->sample_rate);
+
+    DrawLine(0, (i32)center_y, SCREEN_WIDTH, (i32)center_y, (Color){64, 64, 72, 255});
+
+    for (i32 i = 0; i < BAR_COUNT; i++)
+    {
+        i64 start_frame = playhead_frame + ((visible_frames * i) / BAR_COUNT);
+        i64 end_frame = playhead_frame + ((visible_frames * (i + 1)) / BAR_COUNT);
+        i64 frame_count = end_frame - start_frame;
+
+        if (start_frame >= audio->sample_count)
+            break;
+
+        if (frame_count <= 0)
+            frame_count = 1;
+
+        f32 peak = 0.0f;
+        for (i32 channel = 0; channel < audio->channels; channel++)
+        {
+            f32 channel_peak = audio_peak(audio, start_frame, frame_count, channel);
+
+            if (channel_peak > peak)
+                peak = channel_peak;
+        }
+
+        if (peak > 1.0f)
+            peak = 1.0f;
+
+        f32 height = peak * VISUALIZER_MAX_HEIGHT;
+        f32 x = (f32)i * ((f32)SCREEN_WIDTH / (f32)BAR_COUNT);
+        f32 y = center_y - height;
+
+        DrawRectangleRec(
+            (Rectangle){x, y, bar_width, height},
+            (Color){91, 214, 167, 255});
+    }
 }
 
 int main(int argc, char *argv[])
@@ -92,52 +136,32 @@ int main(int argc, char *argv[])
     printf("Format       = %.4s\n", wavheader.format);
 
     InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "lain audio visualizer");
+    InitAudioDevice();
     SetTargetFPS(60);
 
-    const f32 center_y = (f32)SCREEN_HEIGHT / 2.0f;
-    const f32 bar_width = ((f32)SCREEN_WIDTH / (f32)BAR_COUNT) - BAR_GAP;
-    const i64 frames_per_bar = audio.sample_count / BAR_COUNT;
+    Music music = LoadMusicStream(path);
 
     while (!WindowShouldClose())
     {
-        BeginDrawing();
-        ClearBackground((Color){18, 18, 22, 255});
+        SeekMusicStream(music, 0.0f);
+        PlayMusicStream(music);
 
-        DrawLine(0, (i32)center_y, SCREEN_WIDTH, (i32)center_y, (Color){64, 64, 72, 255});
-
-        for (i32 i = 0; i < BAR_COUNT; i++)
+        while (!WindowShouldClose() && IsMusicStreamPlaying(music))
         {
-            i64 start_frame = (audio.sample_count * i) / BAR_COUNT;
-            i64 end_frame = (audio.sample_count * (i + 1)) / BAR_COUNT;
-            i64 frame_count = end_frame - start_frame;
+            UpdateMusicStream(music);
 
-            if (frame_count <= 0)
-                frame_count = frames_per_bar > 0 ? frames_per_bar : 1;
+            BeginDrawing();
+            ClearBackground((Color){18, 18, 22, 255});
 
-            f32 peak = 0.0f;
-            for (i32 channel = 0; channel < audio.channels; channel++)
-            {
-                f32 channel_peak = audio_peak(&audio, start_frame, frame_count, channel);
+            draw_visualizer(&audio, GetMusicTimePlayed(music));
 
-                if (channel_peak > peak)
-                    peak = channel_peak;
-            }
-
-            if (peak > 1.0f)
-                peak = 1.0f;
-
-            f32 height = peak * VISUALIZER_MAX_HEIGHT;
-            f32 x = (f32)i * ((f32)SCREEN_WIDTH / (f32)BAR_COUNT);
-            f32 y = center_y - height;
-
-            DrawRectangleRec(
-                (Rectangle){x, y, bar_width, height},
-                (Color){91, 214, 167, 255});
+            EndDrawing();
         }
-
-        EndDrawing();
     }
 
+    StopMusicStream(music);
+    UnloadMusicStream(music);
+    CloseAudioDevice();
     CloseWindow();
     free(audio.rawdata.data);
 
